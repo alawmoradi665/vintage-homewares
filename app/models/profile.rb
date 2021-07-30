9c3f1f6ad4f2a636fc0499ade7f68d6f939b5fd6
alawmoradi665 <alaw.m@hotmail.com>
2021-07-30 19:07:42 +1000

    models

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_one :location
-  has_many :listings
+  has_many :products_to_purchase, class_name: "Listing", foreign_key: "buyer_id"
+  has_many :products_to_sell, class_name: "Listing", foreign_key: "seller_id" 
 end
