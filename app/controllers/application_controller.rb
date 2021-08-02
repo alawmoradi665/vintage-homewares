b1b3cc61c6100cacf550bf7bdb055b33b8dc485e
alawmoradi665 <alaw.m@hotmail.com>
2021-08-02 15:11:53 +1000

    styling navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-    
+# Creating route from sign up to create profile, every user start off by creating a profile
+    def after_sign_in_path_for(profile)
+        if current_user.profile
+            root_path
+        else
+            redirect_to new_profile_path 
+        end 
+
+    end 
 end
